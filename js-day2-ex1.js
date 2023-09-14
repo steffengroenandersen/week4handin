802fefe8433cb4b3f58aab3a71f6317a56c1abed
steffengroenandersen <steffengroenandersen@gmail.com>
2023-09-14 19:04:31 +0200

    Add day-2 exercises

---
+++ js-day2-ex1.js
@@ -0,0 +1,21 @@
+/*
+    a) Add the three divs below  to an HTML page, and provide each with a unique id*/
+/* 
+    let divs = document.querySelectorAll("div");
+    for(let i = 0; i < divs.length; i++){
+        let div = divs[i];
+        div.style.backgroundColor = "blue";
+    }
+*/
+
+/*
+    let divs = document.querySelectorAll("div");
+    divs.forEach(div => div.style.backgroundColor = "Red");
+*/
+
+/*
+    b) Add a single button, and assign a click handler to the button.
+    When the button is clicked each div should be given a unique colour.
+*/
+
+let button = document.querySelector("#btn1");
